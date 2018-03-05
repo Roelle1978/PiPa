d764987fd20ae0c3dfdc449541733c47d285733a
Micha <Micha@PiPaPo.world>
2018-03-05 16:31:30 +0100

    Bewegtes Logo

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-﻿
-
-
 //class Greeter {
 //    element: HTMLElement;
 //    span: HTMLElement;
 //    timerToken: number;
-
 //    constructor(element: HTMLElement) {
 //        this.element = element;
 //        this.element.innerHTML += "The time is: ";
@@ -13,19 +9,16 @@
 //        this.element.appendChild(this.span);
 //        this.span.innerText = new Date().toUTCString();
 //    }
-
 //    start() {
 //        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
 //    }
-
 //    stop() {
 //        clearTimeout(this.timerToken);
 //    }
-
 //}
-
 //window.onload = () => {
 //    var el = document.getElementById('content');
 //    var greeter = new Greeter(el);
 //    greeter.start();
 //};
+//# sourceMappingURL=app.js.map
